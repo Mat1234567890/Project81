894ea03ff0b6d716d62f63379646b1af782ddc0c
Mat1234567890 <78577538+Mat1234567890@users.noreply.github.com>
2021-08-22 21:25:20 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,42 +3,48 @@ ctx=Canvas.getContext("2d");
 
 var color="grey"
 ctx.beginPath();
+console.log(color);
 ctx.strokeStyle = color;
 ctx.lineWidth = 4;
 ctx.rect(150, 143, 430, 200);
 ctx.stroke();
 
 var color="blue"
-ctx.stokeStyle = color;
 ctx.beginPath();
+ctx.strokeStyle = color;
+console.log(color);
 ctx.lineWidth=5;
 ctx.arc(230,220,40,0,2 * Math.PI);
 ctx.stroke();
 
 var color="yellow"
-ctx.stokeStyle = color;
+console.log(color);
+ctx.strokeStyle = color;
 ctx.beginPath();
 ctx.lineWidth=5;
 ctx.arc(275,265,40,0,2 * Math.PI);
 ctx.stroke();
 
 var color="black"
-ctx.stokeStyle = color;
+console.log(color);
+ctx.strokeStyle = color;
 ctx.beginPath();
 ctx.lineWidth=5;
 ctx.arc(320,220,40,0,2 * Math.PI);
 ctx.stroke();
 
 var color="green"
-ctx.stokeStyle = color;
+console.log(color);
 ctx.beginPath();
+ctx.strokeStyle = color;
 ctx.lineWidth=5;
 ctx.arc(365,265,40,0,2 * Math.PI);
 ctx.stroke();
 
 var color="red"
-ctx.stokeStyle = color;
+console.log(color);
 ctx.beginPath();
+ctx.strokeStyle = color;
 ctx.lineWidth=5;
 ctx.arc(410,220,40,0,2 * Math.PI);
 ctx.stroke();
